6cb01d0d3c9da41d7004ca995afda4b25a60d8a8
supakeen <cmdr@supakeen.com>
2020-06-12 14:44:20 +0200

    Expand relativize tests with success path as well.

--- test/test_safe.py
+++ test/test_safe.py
@@ -4,6 +4,21 @@ import gb.safe
 
 
 @pytest.mark.parametrize(
+    "path0,path1,result",
+    [
+        ("/tmp", "foo", "/tmp/foo"),
+        ("/tmp", "foo/bar", "/tmp/foo/bar"),
+        ("/tmp", "foo/../bar", "/tmp/bar"),
+        ("/tmp", "/foo", "/tmp/foo"),
+        ("/tmp", "/foo/bar", "/tmp/foo/bar"),
+        ("/tmp", "/foo/../bar", "/tmp/bar"),
+    ],
+)
+def test_relativize(path0: str, path1: str, result: str) -> None:
+    assert gb.safe.relativize(path0, path1) == result
+
+
+@pytest.mark.parametrize(
     "path0,path1",
     [
         ("/tmp", "../../../",),
